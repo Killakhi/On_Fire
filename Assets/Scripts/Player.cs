960b7b9554c18811b0f8fd9ebb41b282c28cf136
Killakhi <124313338+Killakhi@users.noreply.github.com>
2024-09-05 03:04:39 -0400

    Created the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float horizontal; //Create float for left to right movement
+    [SerializeField] float horionztalSpeed; //Create the speed variable 
+    public Rigidbody2D rb; //Reference to rigidbody2D
 
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //Get the rigidbody component
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal"); //Get the axis of Horizontal to move left and right
+
+        Vector2 vector = new Vector2(horizontal, 0); //Create a new vector of just going left and right
+        rb.velocity = vector * horionztalSpeed; //Multiply rigidbody with the vector and horionztalSpeed
+
     }
 }
